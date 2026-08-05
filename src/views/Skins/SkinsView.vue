12420dbeeac08f5503a9fdc31139325e8948fa22
洛溪 LoXi <yuansepailin@163.com>
2026-08-05 21:34:10 +0800

    [Add] PageSys

--- src/views/Skins/SkinsView.vue
+++ src/views/Skins/SkinsView.vue
@@ -8,7 +8,25 @@ export default {
   },
   data() {
     return {
-      skinsData: skinsData.skinsData
+      skinsData: skinsData.skinsData,
+      dealSkins: [],
+      switchPage: {
+        totalPage: 0,
+        currentPage: 1,
+        maxPage: 1,
+        maxSkinCount: 20,
+      }
+    }
+  },
+  mounted() {
+    this.switchPage.totalPage = Math.ceil(this.skinsData.skins.length / this.switchPage.maxSkinCount)
+    this.switchPage.maxPage = this.switchPage.totalPage
+    this.switchSkins(0)
+  },
+  methods: {
+    switchSkins(page) {
+      this.switchPage.currentPage += page
+      this.dealSkins = this.skinsData.skins.slice((this.switchPage.currentPage - 1) * this.switchPage.maxSkinCount, (this.switchPage.currentPage) * this.switchPage.maxSkinCount)
     }
   },
 }
@@ -19,17 +37,35 @@ export default {
     <mdui-card class="card">
       <div class="card_title">{{ $t('skins.title') }}</div>
       <div class="card_content">{{ $t('skins.content') }}</div>
-      <div class="card_data">{{$t('skins.skinsAuthor.skinCount') }} {{ skinsData.skins.length }}</div>
+      <div class="card_data">{{ $t('skins.skinsAuthor.skinCount') }} {{ skinsData.skins.length }}</div>
     </mdui-card>
-    <mdui-button full-width variant="elevated" @click="$router.push('/skins/edit')">{{ $t('skins.skinsDetail.button.edit')
+    <mdui-button full-width variant="elevated" @click="$router.push('/skins/edit')">{{
+      $t('skins.skinsDetail.button.edit')
     }}</mdui-button>
     <div class="skins_list">
-      <SkinsCard v-for="skin in skinsData.skins.sort(() => Math.random() - Math.random())" :key="skin.id" :skin="skin" />
+      <SkinsCard v-for="skin in dealSkins" :key="skin.id" :skin="skin" />
+    </div>
+    <div class="add_button">
+      <mdui-button full-width variant="elevated" :disabled="switchPage.currentPage === 1" @click="switchSkins(-1)">
+        {{ $t('skins.skinsDetail.button.prev') }}
+      </mdui-button>
+      <mdui-chip elevated>{{ switchPage.currentPage }}/{{ switchPage.maxPage }}</mdui-chip>
+      <mdui-button full-width variant="elevated" :disabled="switchPage.currentPage === switchPage.maxPage" @click="switchSkins(1)">
+        {{ $t('skins.skinsDetail.button.next') }}
+      </mdui-button>
     </div>
   </div>
 </template>
 
 <style scoped>
+.add_button {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 8px;
+  margin-bottom: 8px;
+}
+
 .skins_list {
   margin-top: 8px;
   display: grid;
